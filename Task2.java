4068e63a324eb945464f5ff3edf7a17a2ac03de9
Чекмарев Денис <chekmarevdenchik5@gmail.com>
2022-10-28 22:11:52 +0700

    task2 is done

--- Task2.java
+++ Task2.java
@@ -1,3 +1,20 @@
 public class Task2 {
+ 
     
+    public static void main(String[] args) {
+        
+        int[] intArray = new int[10];
+        int d = 0;
+
+        try {
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Catching exception: " + e);
+        }
+    }
 }
